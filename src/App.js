30f487b6ac4bae18bf12a0dc7b99c85e535b5aec
Ally4 <el.ally741@gmail.com>
2020-03-25 13:26:35 +0200

    feature(signup):end finishing-of-the-signup [finishes #171971085]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
+import Signup from './components/Signup';
 import './App.css';
 class App extends Component {
   render() {
   return (
-    <div className = 'App'>
+    <div className = 'body'>
       <Nav />
+      <Signup />
     </div>
   );
   }
